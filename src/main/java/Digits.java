2d4e1a63711a662f2b5ec76e708e20bbb83a6a3c
Hombobow <122179708+Hombobow@users.noreply.github.com>
2024-04-05 10:10:32 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,13 +7,36 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+		int count = 0;
+		ArrayList <Integer> nd = new ArrayList <Integer>();
+		while (num /10 > 10) {
+			nd.add(num % 10);
+			num /= 10;
+		}
+		nd.add(num);
+		for (int i = nd.length; i >= 0; i++) {
+			digitList.add(nd.get(i));
+		}
+		
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
+		boolean y = false;
+		int min = digitList.get(0);
+		for (int i = 0; i < digitList.size(); i++) {
+			if (digitList.get(i) < min) {
+				y = true;
+			}
+			else {
+				min = digitList.get(i);
+			}
+		}
+		if (y) {
+			return false;
+		}
+		return true;
 		
-
 	}
 	
 	public String toString()
